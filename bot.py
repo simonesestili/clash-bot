8ff23810c5c307e5a93f12fc0a1458be798275bb
yolodoggy911@gmail.com <75584972+SimoneSestili@users.noreply.github.com>
2021-06-06 23:17:29 -0700

    run

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
-import discord 
+import discord
 from discord.ext import commands
-
 
 bot = commands.Bot(command_prefix=';')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong!')
+
+
+def token():
+    with open('bot_token.txt', 'r') as file:
+        token = file.readline().strip()
+    return token
+
+
+bot.run(token())
